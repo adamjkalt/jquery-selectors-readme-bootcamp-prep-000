b87783ecc4dcdf287a526fabd60294cc8fcfb34d
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-20 18:50:13 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,12 +10,15 @@ return $('img:last')
 
 function ninjaBabySelector() {
 $('img:#baby-ninja')
+return $('img:#baby-ninja')
 };
 
 function divSelector() {
 $('.pics')
+return $('.pics')
 };
 
 function firstListItem() {
 $('ul #pic-list:first-child')
+return $('ul #pic-list:first-child')
 };
