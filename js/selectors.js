5bd224e4167fb7ebc74f84798add7b4f4f0558e3
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-20 18:50:41 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,24 +1,19 @@
 function paragraphSelector() {
-$('p')
 return $('p')
 };
 
 function lastImageSelector() {
-$('img:last')
 return $('img:last')
 };
 
 function ninjaBabySelector() {
-$('img:#baby-ninja')
 return $('img:#baby-ninja')
 };
 
 function divSelector() {
-$('.pics')
 return $('.pics')
 };
 
 function firstListItem() {
-$('ul #pic-list:first-child')
 return $('ul #pic-list:first-child')
 };
